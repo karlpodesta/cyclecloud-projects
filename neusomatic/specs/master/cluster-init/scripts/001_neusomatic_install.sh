750a5636f3b19d913e564eee12891f73c8baae7d
Karl Podesta <karl.podesta@gmail.com>
2018-11-21 01:42:41 +0100

    Changes to install script

--- neusomatic/specs/master/cluster-init/scripts/001_neusomatic_install.sh
+++ neusomatic/specs/master/cluster-init/scripts/001_neusomatic_install.sh
@@ -5,23 +5,23 @@ yum install -y git gcc g++ gcc-c++ zlib zlib-devel docker
 yum update -y nss curl libcurl
 wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
 chmod +x Miniconda2-latest-Linux-x86_64.sh
-./Miniconda2-latest-Linux-x86_64.sh -b
+./Miniconda2-latest-Linux-x86_64.sh -b -y
 export PATH="/root/miniconda2/bin:$PATH"
-cp -rp /root/miniconda2 /mnt/exports/shared/miniconda2
 
 conda install zlib=1.2.11 numpy=1.14.3 scipy=1.1.0 -y
 conda install pytorch=0.3.1 torchvision=0.2.0 cuda80=1.0 -c pytorch -y
 conda install cmake=3.12.1 -c conda-forge -y
 conda install pysam=0.14.1 pybedtools=0.7.10 pytabix=0.0.2 samtools=1.7 tabix=0.2.5 bedtools=2.27.1 biopython=1.68 -c bioconda -y
+cp -rp /root/miniconda2 /mnt/exports/shared/miniconda2
 yum install -y centos-release-scl 
 yum install -y devtoolset-7-gcc* 
-scl enable devtoolset-7 bash 
+echo "cd /mnt/exports/shared; git clone https://github.com/bioinform/neusomatic.git; cd /mnt/exports/shared/neusomatic; ./build.sh" | scl enable devtoolset-7 bash 
 
 ## Install & build Neusomatic code
-cd /mnt/exports/shared
-git clone https://github.com/bioinform/neusomatic.git
-cd /mnt/exports/shared/neusomatic
-./build.sh
+#cd /mnt/exports/shared
+#git clone https://github.com/bioinform/neusomatic.git
+#cd /mnt/exports/shared/neusomatic
+#./build.sh
 
 ## Install AzCopy tool
 cd
@@ -32,7 +32,7 @@ tar -xf azcopy.tar.gz
 
 ## Settings for all users
 chmod -R 777 /mnt/exports/shared
-echo "export PATH=/mnt/exports/shared/miniconda2:$PATH" >> /etc/bashrc
+#echo "export PATH=/mnt/exports/shared/miniconda2:$PATH" >> /etc/bashrc
 
 ## Start docker
 systemctl start docker
